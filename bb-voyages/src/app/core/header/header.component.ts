eafe2c4848eb84b0cf1e4ac4cf22f3f92681189c
Bayryam <bayryambashchoban@gmail.com>
2024-03-22 15:10:40 +0200

    add basic logic for login and logout , navbar update works

--- bb-voyages/src/app/core/header/header.component.ts
+++ bb-voyages/src/app/core/header/header.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  constructor(private userService: UserService) {}
 
+  get isLoggedIn(): boolean {
+    console.log(this.userService.isLoggedIn);
+    return this.userService.isLoggedIn;
+  }
+
+  logout() {
+    this.userService.logout();
+  }
 }
--- bb-voyages/src/app/user/user.service.ts
+++ bb-voyages/src/app/user/user.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  user = {} || undefined;
+  USER_KEY = '[user]';
+  get isLoggedIn(): boolean {
+    return !!this.user;
+  }
+  constructor() {
+    const user = localStorage.getItem(this.USER_KEY) || undefined;
+    if (user) {
+      this.user = JSON.parse(user);
+    }
+  }
 
-  constructor() { }
+  login() {
+    this.user = {
+      username: 'admin',
+      password: 'admin',
+    };
+    localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+  }
+
+  logout() {
+    this.user = undefined;
+    localStorage.removeItem(this.USER_KEY);
+  }
 }
