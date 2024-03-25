46d290e2c10afbf80f390e5a3777f7c87f15d132
Bayryam <bayryambashchoban@gmail.com>
2024-03-25 18:02:53 +0200

    add guards for user-routing and rents-routing

--- bb-voyages/src/app/rents/rents-routing.module.ts
+++ bb-voyages/src/app/rents/rents-routing.module.ts
@@ -3,10 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { CurrentRentComponent } from './current-rent/current-rent.component';
 import { AddRentComponent } from './add-rent/add-rent.component';
 import { RentsListComponent } from './rents-list/rents-list.component';
+import { AuthActivate } from '../guards/auth.activate';
 
 const routes: Routes = [
   { path: 'rent', component: CurrentRentComponent },
-  { path: 'add-rent', component: AddRentComponent },
+  {
+    path: 'add-rent',
+    component: AddRentComponent,
+    canActivate: [AuthActivate],
+  },
   { path: 'rents', component: RentsListComponent },
   { path: 'rents/:id', component: CurrentRentComponent },
 ];
@@ -14,6 +19,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [],
 })
-export class RentsRoutingModule {
-  
-}
+export class RentsRoutingModule {}
